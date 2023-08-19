1f3f1fd5da601b3b8c1004e9cb5c80e5fbc3fc74
David Brunow <david@shepherddog.co>
2023-08-19 14:44:27 -0500

    wip

--- Tests/DesignSystemTests/DesignSystemTests.swift
+++ Tests/DesignSystemTests/DesignSystemTests.swift
@@ -17,7 +17,7 @@ class DesignSystemTests: XCTestCase {
   let ciPath: StaticString = "/Volumes/workspace/repository/ci_scripts/DesignSystemTests.swift"
   let localPath: StaticString = #file
   var isCIEnvironment: Bool {
-    Logger.tests.debug("Current directory path: \(FileManager.default.currentDirectoryPath)")
+    Logger.tests.error("Current directory path: \(FileManager.default.currentDirectoryPath)")
 
     return FileManager.default.currentDirectoryPath.contains("/Volumes/workspace")
 //    ProcessInfo.processInfo.environment["CI"] == "TRUE"
