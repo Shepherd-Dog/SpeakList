dfc449f16f692ce48d1df5835b7303a77f176314
David Brunow <david@shepherddog.co>
2023-11-27 08:42:11 -0600

    wip

--- App/ci_scripts/ci_pre_xcodebuild.sh
+++ App/ci_scripts/ci_pre_xcodebuild.sh
@@ -3,11 +3,13 @@
 brew install jq
 brew install parallel
 
+parallel --citation
+
 # xcrun simctl list -j "devices" | jq -r '.devices | map(.[])[].udid' | parallel 'xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
 
 # xcrun simctl list -j "devices" | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[]) | .[] .udid' | parallel 'xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
 
-xcrun simctl list -j "devices" | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[] | select(.isAvailable == true)) | .[] .udid' | parallel 'xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
+xcrun simctl list -j "devices" | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[] | select(.isAvailable == true)) | .[] .udid' | parallel --jobs 4 'echo {}; xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
 
 exit 0
 
