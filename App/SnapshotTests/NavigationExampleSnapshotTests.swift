8e737bc9d8aa915cf23e3042f4c9f419dd3b4c5d
David Brunow <david@shepherddog.co>
2023-11-27 18:27:57 -0600

    wip

--- App/SnapshotTests/NavigationExampleSnapshotTests.swift
+++ App/SnapshotTests/NavigationExampleSnapshotTests.swift
@@ -1,54 +1,8 @@
-import AccessibilitySnapshot
-import SnapshotTesting
-import SwiftUI
-import XCTest
+//
+//  NavigationExampleSnapshotTests.swift
+//  SnapshotTests
+//
+//  Created by David Brunow on 11/27/23.
+//
 
-class NavigationExampleSnapshotTests: XCTestCase {
-  func testNavigationView() {
-    let view = NavigationView {
-      Text("Text inside a NavigationView")
-        .navigationTitle("Navigation View")
-        .toolbar {
-          ToolbarItem {
-            Button {
-              // no-op
-            } label: {
-              Text("Add")
-            }
-          }
-        }
-    }
-
-    let hostingController = UIHostingController(rootView: view)
-    hostingController.view.frame = UIScreen.main.bounds
-
-    assertSnapshot(
-      of: hostingController,
-      as: .accessibilityImage(drawHierarchyInKeyWindow: true)
-    )
-  }
-
-  func testNavigationStack() {
-    let view = NavigationStack {
-      Text("Text inside a NavigationStack")
-        .navigationTitle("Navigation Stack")
-        .toolbar {
-          ToolbarItem {
-            Button {
-              // no-op
-            } label: {
-              Text("Add")
-            }
-          }
-        }
-    }
-
-    let hostingController = UIHostingController(rootView: view)
-    hostingController.view.frame = UIScreen.main.bounds
-
-    assertSnapshot(
-      of: hostingController,
-      as: .accessibilityImage(drawHierarchyInKeyWindow: true)
-    )
-  }
-}
+import Foundation
