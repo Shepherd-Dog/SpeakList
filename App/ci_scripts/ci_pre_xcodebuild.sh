009fc1563f3902acb0ef07a54129bd95debaf007
David Brunow <david@shepherddog.co>
2023-11-28 08:48:00 -0600

    wip

--- App/ci_scripts/ci_pre_xcodebuild.sh
+++ App/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [[ $CI_XCODEBUILD_ACTION == "test-without-building" ]]
+then
   # Setup the simulators so that they only have one preferred language.
   # This works around an issue where the simulators on Sonoma have multiple
   # preferred languages which include Arabic and therefore provide different
@@ -10,6 +11,8 @@ if [[ $CI_XCODEBUILD_ACTION == "test-without-building" ]]
   brew install jq
   brew install parallel
 
+  # In my testing running more than 2 jobs in parallel led to flaky tests
+  # where the simulators would error out.
   xcrun simctl list -j "devices" \
     | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[] | select(.isAvailable == true)) | .[] .udid' \
     | parallel --jobs 2 'echo {}; xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
