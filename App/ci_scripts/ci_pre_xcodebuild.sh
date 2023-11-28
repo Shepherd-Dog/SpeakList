8358b5dc77b9606eda2732d7b2fca8ca36273018
David Brunow <david@shepherddog.co>
2023-11-28 07:36:30 -0600

    wip

--- App/ci_scripts/ci_pre_xcodebuild.sh
+++ App/ci_scripts/ci_pre_xcodebuild.sh
@@ -11,4 +11,4 @@ brew install parallel
 
 xcrun simctl list -j "devices" \
   | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[] | select(.isAvailable == true)) | .[] .udid' \
-  | parallel --jobs 4 'echo {}; xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
+  | parallel --jobs 2 'echo {}; xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
