dd2d71c4f27a64ef7f5515bc226321ee90c158cc
David Brunow <david@shepherddog.co>
2023-11-28 08:45:04 -0600

    wip

--- App/ci_scripts/ci_pre_xcodebuild.sh
+++ App/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# Setup the simulators so that they only have one preferred language.
-# This works around an issue where the simulators on Sonoma have multiple
-# preferred languages which include Arabic and therefore provide different
-# results when running snapshot tests.
-#
-# This solution was inspired by: https://stackoverflow.com/a/74335552
-brew install jq
-brew install parallel
+if [[ $CI_XCODEBUILD_ACTION == "test-without-building" ]]
+  # Setup the simulators so that they only have one preferred language.
+  # This works around an issue where the simulators on Sonoma have multiple
+  # preferred languages which include Arabic and therefore provide different
+  # results when running snapshot tests.
+  #
+  # This solution was inspired by: https://stackoverflow.com/a/74335552
+  brew install jq
+  brew install parallel
 
-xcrun simctl list -j "devices" \
-  | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[] | select(.isAvailable == true)) | .[] .udid' \
-  | parallel --jobs 2 'echo {}; xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
+  xcrun simctl list -j "devices" \
+    | jq -r '.devices | with_entries(select(.key | contains("iOS"))) | map(.[] | select(.isAvailable == true)) | .[] .udid' \
+    | parallel --jobs 2 'echo {}; xcrun simctl boot {}; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLanguages -array en; xcrun simctl spawn {} defaults write "Apple Global Domain" AppleLocale -string en_US; xcrun simctl shutdown {};'
+fi
