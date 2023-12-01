7e23374be03d4d7125ea6d8960eb349fa228e08d
David Brunow <david@shepherddog.co>
2023-12-01 06:58:33 -0600

    wip

--- App/ci_scripts/ci_pre_xcodebuild.sh
+++ App/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Enable all the macros to run on CI
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
 if [[ $CI_XCODEBUILD_ACTION == "test-without-building" ]]
 then
   # Setup the simulators so that they only have one preferred language.
